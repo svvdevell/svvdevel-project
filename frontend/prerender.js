e77255a7e3bc856ae489f20ff287f76e24dea553
svvdevell <svvdevel@gmail.com>
2025-10-24 18:41:36 +0300

    google

--- frontend/prerender.js
+++ frontend/prerender.js
@@ -97,7 +97,6 @@ function generateHTML(route) {
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
   <!-- Основні мета-теги -->
   <title>${route.title}</title>
   <meta name="description" content="${route.description}">
