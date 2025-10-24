5d1186454bdf475e7647e7ecf2ab2b37cb714633
svvdevell <svvdevel@gmail.com>
2025-10-24 19:07:04 +0300

    push2

--- frontend/generate-sitemap.js
+++ frontend/generate-sitemap.js
@@ -1,7 +1,6 @@
 import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import fetch from 'node-fetch'; // если Node <18
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
