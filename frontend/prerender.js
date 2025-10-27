f5819cf5750d468ee9fad6cce1479b7bd6268f9f
svvdevell <svvdevel@gmail.com>
2025-10-27 19:54:46 +0200

    seo fix 12

--- frontend/prerender.js
+++ frontend/prerender.js
@@ -192,11 +192,11 @@ function generateHTML(route) {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1.0">
                 <!-- Favicon -->
-                <link rel="icon" type="image/x-icon" href="/favicon.ico">
-                <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
-                <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico">
-                <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico">
-                <link rel="icon" type="image/png" sizes="16x16" href="/favicon.ico">
+                <link rel="icon" type="image/x-icon" href="./public/favicon.ico">
+                <link rel="shortcut icon" type="image/x-icon" href="./public/favicon.ico">
+                <link rel="apple-touch-icon" sizes="180x180" href="./public/favicon.ico">
+                <link rel="icon" type="image/png" sizes="32x32" href="./public/favicon.ico">
+                <link rel="icon" type="image/png" sizes="16x16" href="./public/favicon.ico">
 
                 <!-- Основні мета-теги -->
                 <title>${route.title}</title>
