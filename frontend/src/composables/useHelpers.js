c33021a95e6348979bd85d7fdde28ddf6c53c8b9
svvdevel <svvdevel@gmail.com>
2025-11-03 01:08:34 +0200

    fix 1.1

--- frontend/src/composables/useHelpers.js
+++ frontend/src/composables/useHelpers.js
@@ -1,4 +1,4 @@
-export function useFormatters() {
+export function useHelpers() {
     const formatDate = (dateString) => {
         if (!dateString) return ''
         const date = new Date(dateString)
